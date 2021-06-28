944395639138bad3d2e294c297ec81edf67c1acc
Noura Alquran <77915209+Noura-Alquran@users.noreply.github.com>
2021-06-28 21:43:39 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function CookieStandHeader() {
     return (
-        <header className="flex items-center justify-around bg-green-400">
-            <h1 className="py-4 text-4xl font-bold text-center text-gray-800">
+        <header>
+            <h1>
                 Cookie Stand Admin
             </h1>
         
